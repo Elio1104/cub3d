ea55cce73f468ddea42d46bd8cc968bde51156ac
Elio <alondot@gmail.com>
2023-02-20 14:57:44 +0100

    test

--- src/cubed.c
+++ src/cubed.c
@@ -10,11 +10,11 @@ void    get_file(char **argv, t_game *game)
 		ft_error("The map could not be read.", game);
     buf = get_texture(game);
     game->map = get_map(game->fd, buf);
-    //print_map(game->map);
     ret = check_nswe(game);
     if (ret != 0)
         ft_error_code(ret, game);
     check_layout(game);
+    print_map(game->map);
     close(game->fd);
     game->fd = 0;
 }

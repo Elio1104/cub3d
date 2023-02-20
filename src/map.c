dd0db2978e7e9dbaf78dca24913b78cb624ea612
Elio <alondot@gmail.com>
2023-02-20 15:02:11 +0100

    test

--- src/map.c
+++ src/map.c
@@ -13,13 +13,13 @@ void    check_layout(t_game *game)
         {
             if(game->map[i][j] == '0')
             {
-                if(&(game->map[i - 1][j]) && game->map[i][j] == ' ')
+                if(!(&(game->map[i - 1][j])) || (&(game->map[i - 1][j]) && game->map[i - 1][j] == ' '))
                     ft_error_code(WRONG_LAYOUT, game);
-                if(&(game->map[i + 1][j]) && game->map[i][j] == ' ')
+                if(!(&(game->map[i + 1][j])) || (&(game->map[i + 1][j]) && game->map[i + 1][j] == ' '))
                     ft_error_code(WRONG_LAYOUT, game);
-                if(&(game->map[i][j + 1]) && game->map[i][j] == ' ')
+                if(!(&(game->map[i][j + 1])) || (&(game->map[i][j + 1]) && game->map[i][j + 1] == ' '))
                     ft_error_code(WRONG_LAYOUT, game);
-                if(&(game->map[i][j - 1]) && game->map[i][j] == ' ')
+                if(!(&(game->map[i][j - 1])) || (&(game->map[i][j - 1]) && game->map[i][j - 1] == ' '))
                     ft_error_code(WRONG_LAYOUT, game);
             }
             j++;
